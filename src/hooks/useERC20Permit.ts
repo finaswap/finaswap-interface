7457fc43d476e02c6acb8bf24288eaf693c396ba
finaswap <github@finaswap.com>
2021-10-22 13:39:58 +0200

    refactor: rename LP token

--- src/hooks/useERC20Permit.ts
+++ src/hooks/useERC20Permit.ts
@@ -268,7 +268,7 @@ export function useERC20Permit(
 
 const REMOVE_V2_LIQUIDITY_PERMIT_INFO: PermitInfo = {
   version: '1',
-  name: 'SushiSwap LP Token',
+  name: 'FinaSwap LP Token',
   type: PermitType.AMOUNT,
 }
 
