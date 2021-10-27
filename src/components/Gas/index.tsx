de210a1b9c2fd6283632814caf767c1ac444f42e
finaswap <github@finaswap.com>
2021-10-27 12:58:13 +0200

    chore: add gas price for BSC

--- src/components/Gas/index.tsx
+++ src/components/Gas/index.tsx
@@ -1,20 +1,27 @@
 import useSWR, { SWRResponse } from 'swr'
+import { ChainId } from '@finaswap/sdk'
 
 import React from 'react'
 import { t } from '@lingui/macro'
 import { useLingui } from '@lingui/react'
 
-function Gas() {
+function Gas({ network }) {
+  const apiEndpoint =
+    network === ChainId.MAINNET
+      ? 'https://ethgasstation.info/api/ethgasAPI.json?'
+      : 'https://bscgas.info/gas?apikey=bd3301cc74a14eaa98131754ad7baeaa'
+
   const { i18n } = useLingui()
-  const { data, error }: SWRResponse<{ average: number }, Error> = useSWR(
-    'https://ethgasstation.info/api/ethgasAPI.json?',
-    (url) => fetch(url).then((r) => r.json())
+  const { data, error }: SWRResponse<{ average: number; standard: number }, Error> = useSWR(apiEndpoint, (url) =>
+    fetch(url).then((r) => r.json())
   )
 
   if (error) return <div>{i18n._(t`failed to load`)}</div>
   if (!data) return <div>{i18n._(t`loading...`)}</div>
 
-  return <div>{data.average / 10}</div>
+  const gasPrice = network === ChainId.MAINNET ? data.average / 10 : data.standard
+
+  return <div>{gasPrice}</div>
 }
 
 export default Gas
--- src/features/trade/Header.tsx
+++ src/features/trade/Header.tsx
@@ -78,7 +78,7 @@ const ExchangeHeader: FC<ExchangeHeaderProps> = ({ input, output, allowedSlippag
               <MyOrders />
             </div>
           )}
-          {chainId === ChainId.MAINNET && (
+          {(chainId === ChainId.MAINNET || chainId === ChainId.BSC) && (
             <div className="items-center hidden w-full h-full px-3 space-x-3 rounded cursor-pointer text-green text-opacity-80 hover:text-opacity-100 md:flex hover:bg-dark-800">
               <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
@@ -88,7 +88,7 @@ const ExchangeHeader: FC<ExchangeHeaderProps> = ({ input, output, allowedSlippag
               </svg>
 
               <div className="hidden md:block text-baseline">
-                <Gas />
+                <Gas network={chainId} />
               </div>
             </div>
           )}
